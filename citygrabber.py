68827dfd3f126b9be7ef7ef4dc1c93b62d43dde5
Matt Boggie <mboggie@mac.com>
2020-06-05 13:26:13 -0400

    Data refresh script complete; city data updated; data attribution added to footer

--- citygrabber.py
+++ citygrabber.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import json
 from mechanize import Browser
 from bs4 import BeautifulSoup
 
@@ -8,16 +9,24 @@ page = mech.open(url)
 
 html = page.read()
 soup = BeautifulSoup(html)
+#print(soup.table.prettify())
 table = soup.find("table", 'table-condensed')
-
+blob = []
 for row in table.findAll('tr')[0:]:
     col = row.findAll('td')
-    rank = col[0].string
-    city = col[1].find('a').text.split('\n')[0]
-    state = col[2].find('a').text
-    population = str(col[3].text.replace(',', ''))
-    growth = col[4].text.replace(',', '')
+    if col:
+        rank = col[0].string.strip()
+        city = col[1].find('a').text.strip()
+        state = col[2].find('a').text.strip()
+        pop = str(col[3].text.replace(',', '')).strip()
+        #growth = col[4].span.text.strip()
 
-    row = (rank, city, state, population, growth)
-    sep = ','
-    print (sep.join(row))
+        line = '{{"number" : {0}, "city": "{1}", "state" : "{2}", "pop" : {3}}}'.format(rank, city, state, pop)
+        #print(line)
+        blob.append(line)
+        #list.append(entry)
+
+print("[")
+sep = ",\n"
+print(sep.join(blob))
+print("]")
